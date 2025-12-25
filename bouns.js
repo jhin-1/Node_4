9acb44d38ce87a3f368676eedc96f2b2eb1288d5
ahmed yosri ahmed <100616836+jhin-1@users.noreply.github.com>
2025-12-26 00:14:55 +0200

    Update bouns.js remove

--- bouns.js
+++ bouns.js
@@ -1,7 +1,3 @@
 strs = ["flower","flow","flight"]
 console.log(strs.reduce((a, b) => {
-    if(!a || !b) return "";
-    let i = 0;
-    while (a[i] && b[i] && a[i] === b[i]) i++;
-    return a.slice(0, i);
-}))
+    
